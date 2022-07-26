455743a9720958bdf1ef5ac7e78e8bfd30f40981
marcelooo616 <marcelooo2014@hotmail.com>
2022-07-26 17:24:28 -0300

    Base projeto Blog Pessoal

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div >
+        <ul>
+            <li>
+                <a href="home.html">Home</a>
+            </li>
+            <li>
+                <a href="contato.html">Contato</a>
+            </li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
