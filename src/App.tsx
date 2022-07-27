96a6796729db8dd2e2143e070caeeaa4026ef370
marcelooo616 <marcelooo2014@hotmail.com>
2022-07-27 17:11:39 -0300

    adicionando NAVBAR

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 import Home from './paginas/Home';
 
 
 function App() {
   return (
-    <Home/>
+    <>
+       
+       <Navbar/>
+       <Home/>
+       <Footer/>
+    
+    </>
   );
 }
 
--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -1,15 +1,37 @@
 import React from "react";
+import {Box,  Button,  Paper } from "@material-ui/core";
 import "./home/Home.css";
+
 
 function Home(){
     return(
-        /** '  <></> ' - Caminho abreviado fragment que o react utiliza para poder retornar um ou mais elementos*/
+        /** '  <></> ' - Caminho abreviado fragment que o react utiliza para poder retornar um ou mais elementos
         <>
-            <div className="containerHome">
-                <h1>Home</h1>
-                <img src="http://i.imgur.com/H88yIo2.png"  alt="tela inicial"/>
-            </div>
+
+        Exemplo de grid 
+            <Grid container spacing={2}>
+
+                     <Grid item xs={12} sm={8}>
+                        <Paper style={{ height: "100vh", background: "red"}}/>
+                     </Grid>
+
+                <Grid item container direction="column" xs ={12} sm={4} spacing={2}>
+                         <Grid item>
+                            <Paper style={{ height: "49vh", background: "orange"}} />
+                         </Grid>
+                         <Grid item>
+                            <Paper style={{ height: "49vh", background: "green"}}/>
+                         </Grid>
+
+                </Grid>
+           </Grid>
         </>
+        */
+       <>
+
+           <h1>Home</h1>
+       
+       </>
     )
 }
 
